3b64c8d669c505e851fa8b0cbc18be7e1fc225ca
sunjith13 <134358694+sunjith13@users.noreply.github.com>
2023-05-24 07:25:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
+  ['917736720228', 'jithu', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
